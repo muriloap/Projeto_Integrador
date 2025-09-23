94eb1d6ca03d94b1e31ffb0f568f39614bf1552c
muriloap <muprado222@gmail.com>
2025-09-23 17:21:44 -0300

    começo do projeto

--- src/app/components/Assinaturas/index.tsx
+++ src/app/components/Assinaturas/index.tsx
@@ -1,33 +1,27 @@
-import styles from './styles.module.css'
+import Typography from "@mui/material/Typography";
+import Plano from "../Plano";
+import styles from './styles.module.css';
 
 export default function Assinaturas() {
-    return (
-        <>
-        <body>
-            <section id="assinatura">
-            <h1 className={styles.h1}>ASSINATURAS</h1>
-            <span className={styles.corpo}>
-            <div className={styles.tabelaFree}>
-            <h2 className={styles.free}>FREE</h2>
-            <ul className={styles.listaFree}>
-                <li>primeiro</li>
-                <li>segundo</li>
-                <li>terceiro</li>
-                <li>quarto</li>
-            </ul>
-            </div>
-            <div className={styles.tabelaPremium}>
-            <h2 className={styles.premium}>PREMIUM</h2> 
-            <ul className={styles.listaPremium}>
-                <li>primeiro</li>
-                <li>segundo</li>
-                <li>terceiro</li>
-                <li>quarto</li>
-            </ul>
-            </div>
-            </span>
-            </section>
-        </body>
-        </>
-    )
+  return (
+    <>
+      <Typography className={styles.titulo} variant="h3">
+        ASSINATURAS
+      </Typography>
+
+      <div className={styles.container}>
+        <Plano
+          plano="Free"
+          valor={0}
+          descricao={["primeiro", "segundo", "terceiro", "quarto"]}
+        />
+
+        <Plano
+          plano="Premium"
+          valor={49}
+          descricao={["primeiro", "segundo", "terceiro", "quarto"]}
+        />
+      </div>
+    </>
+  );
 }
