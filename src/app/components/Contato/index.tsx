5b346bea6aa50402aebcab060c121dc78b009dce
muriloap <muprado222@gmail.com>
2025-09-25 16:24:34 -0300

    Criação dos componentes do Lading Page

---
+++ src/app/components/Contato/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css'
+
+export default function Contato() {
+    return (
+        <footer className=''>
+            <h2 className={styles.h2}>CONTATO</h2>
+            <div>
+                <p></p>
+            </div>
+        </footer>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import styles from "./styles.module.css";
 import Header from "./components/Header";
 import Assinaturas from "./components/Assinaturas";
+import SobreNos from "./components/SobreNos";
+import Contato from "./components/Contato";
 
 export default function Home() {
   return (
    <>
-   <Header/>
-   <Assinaturas/>
+   <Header />
+   <SobreNos />
+   <Assinaturas />
+   <Contato />
    </>
   );
 }
