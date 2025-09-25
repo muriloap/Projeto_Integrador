726a35a551ec8e6bc27017375d63c3f0329cb125
muriloap <muprado222@gmail.com>
2025-09-25 17:17:24 -0300

    Criação do Landing Page (Contato)

--- src/app/components/Contato/index.tsx
+++ src/app/components/Contato/index.tsx
@@ -2,11 +2,17 @@ import styles from './styles.module.css'
 
 export default function Contato() {
     return (
-        <footer className=''>
-            <h2 className={styles.h2}>CONTATO</h2>
-            <div>
-                <p></p>
+        <>
+        <h2 className={styles.contato}>CONTATO</h2>
+        <span id='contato' className={styles.span}>
+            <div className={styles.divFaleConosco}>
+                <h2 className={styles.faleConosco}>FALE CONOSCO</h2>
+                <p className={styles.p}>Em caso de dúvidas entre em contato conosco clicando no botão abaixo.</p>
             </div>
-        </footer>
+            <div>
+                <h2>CONTATOS</h2>
+            </div>
+        </span>
+        </>
     )
 }
--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -7,7 +7,7 @@ export default function Menu() {
                 <a className={styles.link} href="http://localhost:3000/">Página inicial</a>
                 <a className={styles.link} href="">Sobre nós</a>
                 <a className={styles.link} href="#assinatura">Assinaturas</a>
-                <a className={styles.link} href="">Contato</a>
+                <a className={styles.link} href="#contato">Contato</a>
                 <a className={styles.button} href="">Cadastrar/Entrar</a>
             </div>
         </nav>
