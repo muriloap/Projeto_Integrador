01124fb95d21a18f91fc813751c21e35e42ac458
Murilo Leandro <muriloleandro188@gmail.com>
2025-09-26 11:33:34 -0300

    front user

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,42 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import TextField from '@mui/material/TextField';
 
 export default function Home(){
 
   return(
-    <><Box display="flex" justifyContent="center" alignItems="flex-start" height="100vh" paddingTop={15}>
-      <AccountCircleIcon sx={{ fontSize: 100, color: 'Black' }} />
-    </Box><TextField /></>
+    <Container maxWidth="sm">
 
-  )
+    <Box display="flex" flexDirection="column" alignItems="center" gap={20} mt={4}>
+
+      <AccountCircleIcon sx={{ fontSize: 100, color: 'Black' }} />
+  
+
+    <Container maxWidth="sm">
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2} mt={4}>
+    
+    <Box component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '50ch' } }}
+      noValidate
+      autoComplete="off">
+      <TextField id="outlined-basic" label="Email" variant="outlined" />
+    </Box>
+
+    <Box component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '50ch' } }}
+      noValidate
+      autoComplete="off">
+      <TextField id="outlined-basic"  label="Email" type="password" variant="outlined" />
+    </Box>
+    </Box>
+    
+    </Container>
+    </Box>
+    </Container>
+
+
+)
 }
+    
+    
+  
