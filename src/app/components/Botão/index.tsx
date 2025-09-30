5658ee6dccf8e37461507776f88cec663396dd36
muriloap <muprado222@gmail.com>
2025-09-30 14:59:59 -0300

    alterações

--- src/app/components/Botão/index.tsx
+++ src/app/components/Botão/index.tsx
@@ -7,7 +7,7 @@ type Props = {
   onClick(): void;           
 };
 
-export default function Button(props: Props) {
+export default function Botao(props: Props) {
   const cls = `${styles.btn} ${props.variant === "outline" ? styles["btn-outline"] : styles["btn-primary"]}`;
 
   
