284c8e73f9aff04a401ff573d59cb262dac37cee
Murilo Leandro <muriloleandro188@gmail.com>
2025-10-02 10:41:02 -0300

    Criação sidebar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,7 @@
-import SideBar from "../components/SideBar";
-
+import styles from "./page.module.css"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import HomeIcon from '@mui/icons-material/Home';
 export default function Home() {
 
 
@@ -7,10 +9,41 @@ export default function Home() {
 
 
 
-    
-    return(
-    <>
-    <SideBar/>
-    </>
+
+    return (
+        <div className={styles.containerp}>
+            <nav className={styles.sidebar}>
+                <header>
+                    <div className={styles.imagetext}>
+                        <span className={styles.image}>
+                            <AccountCircleIcon sx={{ fontSize: 50}} />                </span>
+                        <div className={styles.textheadertext}>
+                            <span className={styles.name}>OS Control</span>
+
+                        </div>
+                    </div>
+                    <ChevronRightIcon className={styles.toggle} sx={{fontSize: 30}}/>
+                </header>
+                <div className={styles.menubar}>
+                    <div className={styles.menu}>
+                        <ul className={styles.menulinks}>
+                            <li className={styles.navlink}>
+                                <a href="#">
+                                    <HomeIcon className={styles.icon}></HomeIcon>
+                                    <span className={styles.navtext}>Página Inicial</span>
+                                </a>
+                            </li>
+                            <li className={styles.navlink}>
+                                <a href="#">
+                                    <HomeIcon className={styles.icon}></HomeIcon>
+                                    <span className={styles.navtext}>Página Inicial</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
     )
 }
