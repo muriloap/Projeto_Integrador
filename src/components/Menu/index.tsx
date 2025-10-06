306b2aba38295336b703ade9ff1ebfd2c0d55c2a
muriloap <muprado222@gmail.com>
2025-10-06 16:51:03 -0300

    utilizando o componente Link do Next

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,15 +1,16 @@
-import styles from './styles.module.css'
+import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default function Menu() {
-    return (
-        <nav>
-            <div className={styles.div}>
-                <link className={styles.link} href="/">Página inicial</link>
-                <link className={styles.link} href="#sobrenos">Sobre nós</link>
-                <link className={styles.link} href="#assinatura">Assinaturas</link>
-                <link className={styles.link} href="#contato">Contato</link>
-                <link className={styles.button} href="/login">Cadastrar/Entrar</link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className={styles.div}>
+        <Link className={styles.link} href="/">Página inicial</Link>
+        <Link className={styles.link} href="#sobrenos">Sobre nós</Link>
+        <Link className={styles.link} href="#assinatura">Assinaturas</Link>
+        <Link className={styles.link} href="#contato">Contato</Link>
+        <Link className={styles.button} href="/login">Cadastrar/Entrar</Link>
+      </div>
+    </nav>
+  );
 }
