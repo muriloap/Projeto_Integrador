ec374dd66510915b884ecd92c0027e1b76488d01
Murilo Leandro <muriloleandro188@gmail.com>
2025-10-06 20:56:03 -0300

    Correção do layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,16 +1,30 @@
+'use client'
 import Sidebar from "@/components/SideBar";
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
+import styles from './page.module.css'
 
 
 type Props = {
     children: ReactNode;
 }
 
-export default function HomeLayout(props: Props){
-    return(
+export default function HomeLayout(props: Props) {
+
+    const [open, setIsOpen] = useState(true);
+
+
+    function Mudarcss() {
+        setIsOpen(!open);
+    }
+
+
+    return (
         <>
-            <Sidebar/>
+        <div className={styles.teste}>
+            <div className={`${styles.containerp} ${open ? '' : styles.close}`}/>
+            <Sidebar onClick={Mudarcss} />
             {props.children}
+        </div>
         </>
     )
 }
