dd4da9ef0b6db82cb487b6534d18888189e3dcc0
Murilo Leandro <muriloleandro188@gmail.com>
2025-10-10 17:22:28 -0300

    logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,6 +2,6 @@ import styles from './styles.module.css'
 
 export default function Logo() {
     return (
-        <img className={styles.img} width="70px" height="auto" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuL0GtlBmwphSD6sKaNFNBXQ7AjHpJE-yUZg&s" alt="Imagem Matato" />
+        <img className={styles.img} width="70px" height="auto" src="./images/logo.jpg" alt="Logo Empresa" />
     )
 }
