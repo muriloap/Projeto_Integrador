19a4fa9144c627c34882eaf1e0d17db1fe7d0f12
Murilo Leandro <muriloleandro188@gmail.com>
2025-11-23 21:58:27 -0300

    Grafico de barras

--- src/app/home/relatorios/page.tsx
+++ src/app/home/relatorios/page.tsx
@@ -8,7 +8,11 @@ import Order from '@/models/order';
 import Product from '@/models/product';
 import Service from '@/models/service';
 import Client from '@/models/client';
-import GraphicPizza from '@/components/GraphicPizza';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS } from "chart.js/auto";
+import { Chart, BarElement, CategoryScale, LinearScale, ArcElement, Tooltip, Legend, defaults } from "chart.js";
+
+Chart.register(BarElement, CategoryScale, LinearScale, ArcElement, Tooltip, Legend);
 
 export default function PageRelatorios() {
 
@@ -100,6 +104,19 @@ export default function PageRelatorios() {
             .catch(loadFalha);
     };
 
+    function getNextMonths(count: number) {
+        const meses = [];
+        const data = new Date();
+
+        for (let i = 0; i < count; i++) {
+            const mes = data.toLocaleString("pt-BR", { month: "long" });
+            meses.push(mes);
+            data.setMonth(data.getMonth() + 1);
+        }
+
+        return meses;
+    }
+
     useEffect(() => {
         loadOrder();
         loadClient();
@@ -146,6 +163,62 @@ export default function PageRelatorios() {
 
     });
 
+    const labels = getNextMonths(7);
+
+    defaults.maintainAspectRatio = true;
+    defaults.responsive = true;
+
+    ChartJS.defaults.plugins.title.display = true;
+    ChartJS.defaults.plugins.title.align = "center";
+    ChartJS.defaults.plugins.title.color = "black";
+
+
+
+    const dataBar = {
+        labels: labels,
+        datasets: [
+            {
+                data: [order.length],
+                label: 'Fluxo de entrada',
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }
+
+        ],
+        options: {
+            plugins: {
+                title: {
+                    text: 'Custom Chart Title'
+                }
+            }
+        }
+    };
+
+    const optionsBar = {
+        plugins: {
+            title: {
+                text: 'Fluxo de Entrada Mensal'
+            }
+        }
+    };
+
 
 
 
@@ -163,17 +236,102 @@ export default function PageRelatorios() {
                     <div className={styles.graphic}>
 
                         <div className={styles.pizza}>
-                            <GraphicPizza
-                                finalizada={filteredOrdersFinalizado.length}
-                                cancelados={filteredOrdersCancelados.length}
-                                total={order.length} />
+                            <Doughnut data={{
+                                labels: ["Total de Os", "Cancelados", "Finalizadas"],
+                                datasets: [
+                                    {
+                                        data: [order.length, filteredOrdersCancelados.length, filteredOrdersCancelados.length],
+                                        backgroundColor: ["rgba(0, 89, 255, 0.8)", "rgba(255, 0, 0, 0.8)", "rgba(0, 255, 13, 0.8)"],
+                                        borderRadius: 5,
+                                    },
+                                ],
+                            }}
+                                options={{
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            align: "start",
+                                            text: 'Controle de Ordem de Serviço',
+                                            font: {
+                                                size: 20
+                                            }
+                                        },
+
+                                        legend: {
+                                            display: true,
+                                            position: "right",
+                                            align: "end",
+                                            textDirection: "left",
+                                            labels: {
+                                                font: {
+                                                    size: 14
+                                                },
+                                            },
+                                        },
+                                    },
+                                }}
+                            />
                         </div>
 
                         <div className={styles.barra}>
-                            <GraphicPizza
-                                finalizada={filteredOrdersFinalizado.length}
-                                cancelados={filteredOrdersCancelados.length}
-                                total={order.length} />
+
+                            <Bar data={{
+                                labels: labels,
+                                datasets: [
+                                    {
+                                        data: [order.length],
+                                        backgroundColor: [
+                                            'rgba(255, 99, 132, 0.2)',
+                                            'rgba(255, 159, 64, 0.2)',
+                                            'rgba(255, 205, 86, 0.2)',
+                                            'rgba(75, 192, 192, 0.2)',
+                                            'rgba(54, 162, 235, 0.2)',
+                                            'rgba(153, 102, 255, 0.2)',
+                                            'rgba(201, 203, 207, 0.2)'
+                                        ],
+                                        borderColor: [
+                                            'rgb(255, 99, 132)',
+                                            'rgb(255, 159, 64)',
+                                            'rgb(255, 205, 86)',
+                                            'rgb(75, 192, 192)',
+                                            'rgb(54, 162, 235)',
+                                            'rgb(153, 102, 255)',
+                                            'rgb(201, 203, 207)'
+                                        ],
+                                        borderWidth: 1
+                                    }
+                                ],
+                            }}
+                                options={{
+                                    layout: {
+                                        padding:{
+                                            bottom: 0
+                                        },
+                                    },
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            padding: 5,
+                                            position: "top",
+                                            text: 'Controle de Ordem de Serviço',
+                                            font: {
+                                                size: 20
+                                            },
+                                            
+                                        },
+
+                                        legend: {
+                                            display: false,
+                                            labels: {
+                                                font: {
+                                                    size: 14
+                                                },
+                                            },
+                                        },
+                                    },
+                                }}
+                            
+                            />
 
                         </div>
 
